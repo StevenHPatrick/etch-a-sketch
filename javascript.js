0712a52486296ff3e88a0e2a3d6d5ac31bfc550f
Steven Patrick <spatrick96@posteo.net>
2025-04-09 20:09:19 -0700

    Completed Etch-A-Sketch
    -Created a grid for the etch-a-sketch in the javascript
    -Added modifing the grid colors
    -Added buttons to clear and create the grid
    -Added randomization of the colors when interacting with the grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,56 @@
-const body = document.body;
+const body = document.querySelector(".etch");
+const create = document.querySelector(".grid");
+const clear = document.querySelector(".clear");
+
 const div = document.createElement("div");
 
-for (let i = 0; i < 16; i++){
-  div.textContent = ".";
-  for (let j = 0; j < 16; j++){
-    const clone = div.cloneNode();
-    clone.textContent = ':';
-    div.appendChild(clone);
-  }
-  body.appendChild(div.cloneNode(true));
-  div.innerHTML = "";
+let num = 0;
+
+function clearGrid(){
+  body.innerHTML = "";
 }
+
+function createGrid(){
+  
+  div.classList.add("row");
+
+  for (let i = 0; i < num; i++){
+    div.classList.toggle("row");
+    // div.textContent = i;
+    for (let j = 0; j < num; j++){
+      const clone = div.cloneNode();
+      clone.classList.add("items");
+      clone.textContent = j;
+      div.appendChild(clone);
+    }
+    div.classList.toggle("row");
+    body.appendChild(div.cloneNode(true));
+    div.innerHTML = "";
+  }
+
+
+  const items = document.querySelectorAll(".items");
+  items.forEach(addToggle);  
+}
+
+function addToggle(item){
+  item.addEventListener("mouseover", () => {
+    let color = Math.floor(Math.random() * 1000000);
+    console.log(color);
+    item.style.backgroundColor = "#" + color;
+  });
+}
+
+
+create.addEventListener("click", () => {
+  do {
+    num = prompt("How big? (<= 100)");  
+  } while ((num <= 0) && (num > 100));
+  clearGrid();
+  createGrid();
+});
+
+clear.addEventListener("click", () => {
+  clearGrid();
+  createGrid();
+});
